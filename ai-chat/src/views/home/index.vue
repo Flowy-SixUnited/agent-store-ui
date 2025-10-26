47556def6482dcbc920fa309632b5b1219ee8526
刘嘉欣 <2312753782@qq.com>
2025-10-27 00:11:59 +0800

    feat:初始化智能客服

---
+++ ai-chat/src/views/home/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main bg-[#fcfcfc]">
+    <div class="title">你好，开始美好的一天！</div>
+    <div class="content">
+      <el-input
+        v-model="textarea"
+        :rows="4"
+        type="textarea"
+        placeholder="请问我有什么能帮助你的？"
+      />
+      <div class="flex items-center justify-between">
+        <div
+          class="flex items-center justify-center bg-[#F0F0F0] h-8 w-8 rounded-[50px]"
+        >
+          <el-icon><Plus /></el-icon>
+        </div>
+        <img class="h-8 w-8" src="@/assets/home/disable-send.png" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Plus } from "@element-plus/icons-vue";
+defineOptions({
+  name: "home",
+});
+const textarea = ref("");
+</script>
+<style scoped lang="scss">
+.main {
+  height: 100vh;
+}
+.title {
+  padding-top: 172px;
+  font-family: HarmonyOS Sans SC, HarmonyOS Sans SC;
+  font-weight: 700;
+  font-size: 32px;
+  color: #1f2937;
+  line-height: 32px;
+  text-align: center;
+}
+.content {
+  margin: 40px auto;
+  width: 800px;
+  height: 160px;
+  background: #ffffff;
+  box-shadow: 0px 0px 10px 0px rgba(176, 198, 212, 0.3);
+  border-radius: 12px;
+  padding: 16px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
